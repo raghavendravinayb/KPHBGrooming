694f201d1705531cbf9d8974806c24ae2b230198
Qspiders-Swiping Pc <Qspiders-Swiping Pc@192.168.0.38>
2021-03-01 15:23:03 +0530

    Project Commit

---
+++ FlipKart/src/test/java/com/flipkart/cart/Demo.java
@@ -0,0 +1,13 @@
+package com.flipkart.cart;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testMethod()
+	{
+		Reporter.log("Demo Running Succesffully",true);
+	}
+}
---
+++ FlipKart/src/test/java/com/flipkart/products/Remo.java
@@ -0,0 +1,13 @@
+package com.flipkart.products;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+
+	@Test
+	public void testMethod()
+	{
+		Reporter.log("Remo Running Succesffully",true);
+	}
+}
